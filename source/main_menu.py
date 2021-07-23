8850dac19039107620bb6a72859fe5f96294d974
Nicolas Yan <nicolas07.yan@gmail.com>
2021-07-23 17:51:29 -0300

    Background added to main menu

--- source/main_menu.py
+++ source/main_menu.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 
 class MainMenu(tk.Frame):
@@ -9,7 +10,18 @@ class MainMenu(tk.Frame):
         master.geometry(f"{self.width}x{self.height}")
         master.resizable(False, False)
         master.title("Master Chess")
+        self.canvas = tk.Canvas(master, width=self.width, height=self.height)
+        self.canvas.pack()
         self.master = master
+        self.set_background()
+
+    def set_background(self):
+        img_path = "images/background.png"
+        img = Image.open(img_path)
+        resized_img = img.resize((self.width, self.height), Image.ANTIALIAS)
+        background = ImageTk.PhotoImage(resized_img)
+        self.canvas.background = background
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=background)
 
 
 def main():
