f2c9e14dffc4500d2b3e806a854e107760d1769c
Nicolas Yan <nicolas07.yan@gmail.com>
2021-09-23 08:42:08 -0300

    Load game window layout

--- source/load_game.py
+++ source/load_game.py
@@ -9,6 +9,22 @@ class LoadGameWindow(tk.Frame):
         master.resizable(False, False)
         master.title("Load Game")
         self.master = master
+        self.pack(expand=True, fill=tk.BOTH)
+        self.set_components()
+
+    def set_components(self):
+        self.set_listbox()
+
+    def set_listbox(self):
+        listbox_frame = tk.Frame(self, width=240, height=590)
+        listbox_frame.pack_propagate(False)
+        listbox_frame.pack(pady=5, padx=5, side=tk.LEFT)
+        listbox = tk.Listbox(listbox_frame, width=28)
+        listbox.pack(side=tk.LEFT, anchor=tk.NW, fill=tk.BOTH)
+        scrollbar = tk.Scrollbar(listbox_frame)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        listbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=listbox.yview)
 
 
 if __name__ == '__main__':
